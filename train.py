5d668a7e5ff1a004c280edff1361fa6e4f58be53
hanghangzhou9520 <18189531019@163.com>
2025-11-04 18:08:45 +0800

    train.py

--- train.py
+++ train.py
@@ -1,10 +1,8 @@
 import os
-
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import os
 import pickle
-import torch.nn as nn
-from torch.utils.data import dataloader, dataset
+from torch.utils.data import dataloader
 import torch.optim as optim
 import numpy as np
 import argparse
@@ -16,7 +14,6 @@ import glob
 import hashlib
 
 parser = argparse.ArgumentParser()
-
 # Model specific parameters
 parser.add_argument('--input_size', type=int, default=2)
 parser.add_argument('--att_num', type=int, default=2)
@@ -26,47 +23,28 @@ parser.add_argument('--n_txpcnn', type=int, default=5, help='Number of TXPCNN la
 parser.add_argument('--learn_A', action="store_false", default=True, help='Self learning Adjacecny matrix')
 parser.add_argument('--video_back', action="store_true", default=False, help='Use Sequence of image embedding')
 parser.add_argument('--background_back', action="store_true", default=False, help='Use single image embedding ')
-
 # Data specifc paremeters
 parser.add_argument('--obs_seq_len', type=int, default=5)  # How many corresponds the paper?
 parser.add_argument('--pred_seq_len', type=int, default=10)
-parser.add_argument('--dataset', default='GTA_IM',
-                    help='PROX,GTA_IM')
+parser.add_argument('--dataset', default='GTA_IM', help='PROX,GTA_IM')
 parser.add_argument('--im_w', type=int, default=30)
 parser.add_argument('--im_h', type=int, default=60)
-
 # loss function sepecific parameters
 parser.add_argument('--use_cons', action="store_true", default=False, help='Use consistently loss')
-parser.add_argument('--l_norm', type=float, default=0.01,
-                    help='weight of norm')
-parser.add_argument('--l_cos', type=float, default=0.01,
-                    help='weight of cos similarity')
-
+parser.add_argument('--l_norm', type=float, default=0.01, help='weight of norm')
+parser.add_argument('--l_cos', type=float, default=0.01, help='weight of cos similarity')
 # Training specifc parameters
-parser.add_argument('--log_frq', type=int, default=32,
-                    help='frequency of logging')
-parser.add_argument('--batch_size', type=int, default=128,
-                    help='minibatch size')
-parser.add_argument('--num_epochs', type=int, default=200,
-                    help='number of epochs')
-parser.add_argument('--clip_grad', type=float, default=None,
-                    help='gadient clipping')
-parser.add_argument('--lr', type=float, default=0.01,
-                    help='learning rate')
-parser.add_argument('--lr_sh_rate', type=int, default=100,
-                    help='number of steps to drop the lr')
-parser.add_argument('--use_lrschd', action="store_true", default=False,
-                    help='Use lr rate scheduler')
-parser.add_argument('--tag', default='NA',
-                    help='Personal tag epx')
-
-# misc
+parser.add_argument('--log_frq', type=int, default=32, help='frequency of logging')
+parser.add_argument('--batch_size', type=int, default=128, help='minibatch size')
+parser.add_argument('--num_epochs', type=int, default=200, help='number of epochs')
+parser.add_argument('--clip_grad', type=float, default=None, help='gadient clipping')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+parser.add_argument('--lr_sh_rate', type=int, default=100, help='number of steps to drop the lr')
+parser.add_argument('--use_lrschd', action="store_true", default=False, help='Use lr rate scheduler')
+parser.add_argument('--tag', default='NA', help='Personal tag epx')
 parser.add_argument('--eval_only', action="store_true", default=False, help='evaluate the model')
-parser.add_argument('--torso_joint', type=int, default=13,
-                    help='center of torso, 13 for GTA-IM')  # 19 16
-
+parser.add_argument('--torso_joint', type=int, default=13, help='center of torso, 13 for GTA-IM')  # 19 16
 args = parser.parse_args()
-
 # create a unique tag per exp
 original_tag = args.tag
 
@@ -91,7 +69,6 @@ if args.dataset == 'GTA_IM':
     dataset_test = D2_D3_GTA_IM(datasections[8:10], tag='test',
                                 seq_in=args.obs_seq_len, seq_out=args.pred_seq_len, load_img=load_img, load_depth=False,
                                 img_resize=(args.im_w, args.im_h))
-
 
 elif args.dataset == 'PROX':
     record_sections = glob.glob('PROX/recordings/*/')
@@ -121,19 +98,15 @@ loader_val = torch.utils.data.DataLoader(
 # normalization
 all_train_2d = []
 all_test_3d = []
-
 for cnt, batch in enumerate(loader_train):
     if args.background_back or args.video_back:
         X, MA, y, scene_l, scene_g = batch
     else:
         X, MA, y = batch
-
     all_train_2d.extend(X.flatten().numpy())
     all_test_3d.extend(y.flatten().numpy())
-
 all_train_2d = np.asarray(all_train_2d)
 all_test_3d = np.asarray(all_test_3d)
-
 _mean = all_train_2d.mean()
 _std = all_train_2d.std()
 y_mean = all_test_3d.mean()
@@ -152,24 +125,18 @@ model = PhysIRL(n_stgcnn=args.n_stgcnn, n_txpcnn=args.n_txpcnn,
                 in_channels=args.input_size, out_channels=args.output_size,
                 seq_len=args.obs_seq_len, pred_seq_len=args.pred_seq_len, kernel_size=3,
                 learn_A=args.learn_A, video_back=args.video_back, background_back=args.background_back).cuda()
-
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 total_params = sum(p.numel() for p in model.parameters())
 print(f"{total_params / 1000:.3f}k")
 if args.use_lrschd:
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_sh_rate, gamma=0.2)
-
 checkpoint_dir = './checkpoint/' + args.tag + '/'
-
 if not os.path.exists(checkpoint_dir):
     os.makedirs(checkpoint_dir)
-
 with open(checkpoint_dir + 'args.pkl', 'wb') as fp:
     pickle.dump(args, fp)
-
 print('Data and model loaded')
 print('Checkpoint dir:', checkpoint_dir)
-
 
 # train val functions
 def train(epoch):
@@ -182,25 +149,17 @@ def train(epoch):
         # get data
         if args.background_back:
             X, MA, y, scene_l, scene_g = batch
-            #             scene[...,0] = (scene[...,0]-v_mean[0])/v_std[0]
-            #             scene[...,1] = (scene[...,1]-v_mean[1])/v_std[1]
-            #             scene[...,2] = (scene[...,2]-v_mean[2])/v_std[2]
             scene_l = scene_l[:, -1, ...].cuda()
             scene_g = scene_g[:, -1, ...].cuda()
         elif args.video_back:
             X, MA, y, scene_l, scene_g = batch
-            #             scene[...,0] = (scene[...,0]-v_mean[0])/v_std[0]
-            #             scene[...,1] = (scene[...,1]-v_mean[1])/v_std[1]
-            #             scene[...,2] = (scene[...,2]-v_mean[2])/v_std[2]
             scene_l = scene_l.view(scene_l.shape[0], scene_l.shape[1] * scene_l.shape[2], scene_l.shape[3],
                                    scene_l.shape[4]).cuda()
             scene_g = scene_g.view(scene_g.shape[0], scene_g.shape[1] * scene_g.shape[2], scene_g.shape[3],
                                    scene_g.shape[4]).cuda()
             MA = MA.view(MA.shape[0], MA.shape[1] * MA.shape[2], MA.shape[3], MA.shape[4]).transpose(2, 3).cuda()
-        #             scene = scene.transpose(1,2)
         else:
             X, MA, y = batch
-
         # X = (X - _mean) / _std
         # y = (y - y_mean) / y_std
         # y__ = y_*y_std + y_mean
@@ -211,21 +170,16 @@ def train(epoch):
             V_pred, m = model(X, MA, scene_l, scene_g)
         else:
             V_pred = model(X, MA)
-
-        # loss = MPJPE(V_pred, y)# * 0.5 + NL(V_pred, y, m)*0.5
-        loss = kinetic_energy_conservation_loss(V_pred, y, m)
+        loss = ECLoss(V_pred, y, m)
         loss.backward()
         if args.clip_grad is not None:
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad)
         optimizer.step()
-
         # Logging
         loss_train += loss.item()
         if cnt % args.log_frq == 0 and cnt != 0:
             print('Epoch:', epoch, '\t Train Loss:', loss_train / (cnt + 1))
-
     metrics['train_loss'].append(loss_train / (cnt + 1))
-
 
 def vald(epoch):
     global metrics, constant_metrics
@@ -238,43 +192,32 @@ def vald(epoch):
             # get data
             if args.background_back:
                 X, MA, y, scene_l, scene_g = batch
-                #             scene[...,0] = (scene[...,0]-v_mean[0])/v_std[0]
-                #             scene[...,1] = (scene[...,1]-v_mean[1])/v_std[1]
-                #             scene[...,2] = (scene[...,2]-v_mean[2])/v_std[2]
                 scene_l = scene_l[:, -1, ...].cuda()
                 scene_g = scene_g[:, -1, ...].cuda()
             elif args.video_back:
                 X, MA, y, scene_l, scene_g = batch
-                #             scene[...,0] = (scene[...,0]-v_mean[0])/v_std[0]
-                #             scene[...,1] = (scene[...,1]-v_mean[1])/v_std[1]
-                #             scene[...,2] = (scene[...,2]-v_mean[2])/v_std[2]
                 scene_l = scene_l.view(scene_l.shape[0], scene_l.shape[1] * scene_l.shape[2], scene_l.shape[3],
                                        scene_l.shape[4]).cuda()
                 scene_g = scene_g.view(scene_g.shape[0], scene_g.shape[1] * scene_g.shape[2], scene_g.shape[3],
                                        scene_g.shape[4]).cuda()
                 MA = MA.view(MA.shape[0], MA.shape[1] * MA.shape[2], MA.shape[3], MA.shape[4]).transpose(2, 3).cuda()
-            #             scene = scene.transpose(1,2)
             else:
                 X, MA, y = batch
-
             # X = (X - _mean) / _std
             # y = (y - y_mean) / y_std
             X, y = X.cuda(), y.cuda()
-
             # Forward, loss, eval metrics
             if args.background_back or args.video_back:
                 V_pred, m = model(X, MA, scene_l, scene_g)
             else:
                 V_pred = model(X, MA)
-            loss = kinetic_energy_conservation_loss(V_pred, y, m)
-
+            loss = ECLoss(V_pred, y, m)
             mpjpe = MPJPE(V_pred, y)
             # Logging
             loss_val += loss.item()
             mpjpe_avg += mpjpe.item()
             if cnt % args.log_frq == 0 and cnt != 0:
                 print('Epoch:', epoch, '\t Val Loss:', loss_val / (cnt + 1), '\t MPJPE:', mpjpe_avg / (cnt + 1))
-
     metrics['val_loss'].append(loss_val / (cnt + 1))
     metrics['val_mpjpe'].append(mpjpe_avg / (cnt + 1))
 
@@ -287,18 +230,6 @@ def vald(epoch):
         constant_metrics['min_val_mpjpe'] = metrics['val_mpjpe'][-1]
         constant_metrics['min_val_mpjpe_epoch'] = epoch
         torch.save(model.state_dict(), checkpoint_dir + 'val_mpjpe_best.pth')
-
-
-def multi_range_ratio1(tensor_list, ranges):
-    combined = torch.cat([t.flatten() for t in tensor_list])
-    results = {}
-    for (a, b) in ranges:
-        mask = (combined >= a) & (combined < b)
-        results[f"[{a}, {b})"] = mask.sum().item() / combined.numel()
-    sum_ratios = sum(results.values())
-    print(f"Sum of ratios: {sum_ratios:.2f}")  # 输出 Sum of ratios: 1.00
-    return results
-
 
 def multi_range_ratio(tensor_list, ranges):
     """
@@ -328,7 +259,6 @@ def multi_range_ratio(tensor_list, ranges):
     total_percentage = round(sum(float(p[:-1]) for p in percentages.values()), 2)
     print(total_percentage)
     return percentages
-
 
 # Train or eval?
 if not args.eval_only:
@@ -384,23 +314,16 @@ else:
                 # get data
                 if args.background_back:
                     X, MA, y, scene_l, scene_g = batch
-                    #             scene[...,0] = (scene[...,0]-v_mean[0])/v_std[0]
-                    #             scene[...,1] = (scene[...,1]-v_mean[1])/v_std[1]
-                    #             scene[...,2] = (scene[...,2]-v_mean[2])/v_std[2]
                     scene_l = scene_l[:, -1, ...].cuda()
                     scene_g = scene_g[:, -1, ...].cuda()
                 elif args.video_back:
                     X, MA, y, scene_l, scene_g = batch
-                    #             scene[...,0] = (scene[...,0]-v_mean[0])/v_std[0]
-                    #             scene[...,1] = (scene[...,1]-v_mean[1])/v_std[1]
-                    #             scene[...,2] = (scene[...,2]-v_mean[2])/v_std[2]
                     scene_l = scene_l.view(scene_l.shape[0], scene_l.shape[1] * scene_l.shape[2], scene_l.shape[3],
                                            scene_l.shape[4]).cuda()
                     scene_g = scene_g.view(scene_g.shape[0], scene_g.shape[1] * scene_g.shape[2], scene_g.shape[3],
                                            scene_g.shape[4]).cuda()
                     MA = MA.view(MA.shape[0], MA.shape[1] * MA.shape[2], MA.shape[3], MA.shape[4]).transpose(2,
                                                                                                              3).cuda()
-                #             scene = scene.transpose(1,2)
                 else:
                     X, MA, y = batch
                 # y = (y - y_mean) / y_std
@@ -413,7 +336,7 @@ else:
                 else:
                     V_pred = model(X, MA)
 
-                loss_val += kinetic_energy_conservation_loss(V_pred, y, m).item()
+                loss_val += ECLoss(V_pred, y, m).item()
 
                 mpjpe_avg += MPJPE(V_pred, y).item()
                 # print(mpjpe_avg)
@@ -498,7 +421,6 @@ else:
             eval_result_row += str(ss)
         f.write(eval_result_row)
         f.close()
-
 
     # Eval at differnet time steps and 3d pose, 3d positions
     # Load the model weights
